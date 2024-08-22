e2f060c0cbbfa3b561173351d777301d153d4a94
SurajNyavanandi <surajdec11@gmail.com>
2024-08-22 15:25:39 +0530

    Added LoginPage(Backend) Feature

--- ChatApp/controllers/user.js
+++ ChatApp/controllers/user.js
@@ -1,6 +1,7 @@
 const User = require('../models/userModel');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/database');
+const jwt=require('jsonwebtoken');
 
 exports.createUser = async (req, res) => {
     const t = await sequelize.transaction();
@@ -22,3 +23,24 @@ exports.createUser = async (req, res) => {
         return res.status(500).json({ Error: 'Error Signing Up User' });
     }
 };
+exports.createLogin = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ where: { email: email } });
+
+        if (!user) {
+            return res.status(404).json({ Error: 'User not found' });
+        }
+
+        const passMatch = await bcrypt.compare(password, user.password);
+        if (!passMatch) {
+            return res.status(401).json({ Error: 'Incorrect Password' });
+        }
+
+        const token = jwt.sign({ id: user.id }, 'secretchat@key');
+        return res.status(200).json({ Message: 'Login Successful',token});
+
+    } catch (error) {
+        return res.status(500).json({ Error: 'Error creating login details' });
+    }
+}
--- ChatApp/routes/userRouter.js
+++ ChatApp/routes/userRouter.js
@@ -3,5 +3,6 @@ const userController = require('../controllers/user');
 const router = express.Router();
 
 router.post('/signup', userController.createUser);
+router.post('/login',userController.createLogin);
 
 module.exports = router;
