255bd9fa3211a953e61c0eeb0869d558406b0c95
SurajNyavanandi <surajdec11@gmail.com>
2024-08-22 12:39:48 +0530

    Added code for SignUp(Backend)

--- ChatApp/app.js
+++ ChatApp/app.js
@@ -1,12 +1,19 @@
 const express=require('express');
+const sequelize = require('./config/database');
+const path = require('path');
+const bodyParser = require('body-parser');
+const userRouter = require('./routes/userRouter');
 const app=express();
 
-
+app.use(bodyParser.json());
+app.use('/user', userRouter);
 
 app.get('/signup',(req,res)=>{
     res.sendFile(path.join(__dirname,'views','signup.html'));
 })
-app.listen(1500,()=>{
-    console.log("ChatApp started running on port 1500");
-    
-});
+sequelize.sync()
+    .then(() => {
+        console.log("Database Connected");
+        app.listen(1500, () => console.log("Server running on http://localhost:1500/signup"));
+    })
+    .catch(err => console.log("Database Not-Connected", err));
---
+++ ChatApp/controllers/user.js
@@ -0,0 +1,24 @@
+const User = require('../models/userModel');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/database');
+
+exports.createUser = async (req, res) => {
+    const t = await sequelize.transaction();
+    try {
+        const { name, email, phone, password } = req.body;
+
+        const existingUser = await User.findOne({ where: { email } });
+        if (existingUser) {
+            return res.status(400).json({ Error: 'User already exists' });
+        }
+        const hashPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({ name, email, phone, password: hashPassword }, { transaction: t });
+
+        await t.commit();
+        return res.status(201).json({ user, Message: 'SignUp Successful' });
+    } catch (error) {
+        await t.rollback();
+        console.error('Error Signing Up User:', error);
+        return res.status(500).json({ Error: 'Error Signing Up User' });
+    }
+};
