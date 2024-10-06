4d6bbf7f7b5515627ab4c3bc95647a832cb2a81a
SurajNyavanandi <surajdec11@gmail.com>
2024-10-06 15:15:49 +0530

    deleting items from the cart

--- MongoDB/controllers/shop.js
+++ MongoDB/controllers/shop.js
@@ -72,12 +72,13 @@ exports.postCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-    .deleteItemFromCart(prodId)
+    .removeFromCart(prodId)
     .then(result => {
       res.redirect('/cart');
     })
     .catch(err => console.log(err));
 };
+
 
 exports.postOrder = (req, res, next) => {
   let fetchedCart;
--- MongoDB/models/user.js
+++ MongoDB/models/user.js
@@ -49,3 +49,18 @@ userSchema.methods.addToCart = function(product) {
 };
 
 module.exports = mongoose.model('User', userSchema);
+
+userSchema.methods.removeFromCart = function(productId) {
+  const updatedCartItems = this.cart.items.filter(item => {
+    return item.productId.toString() !== productId.toString();
+  });
+  this.cart.items = updatedCartItems;
+  return this.save();
+};
+
+userSchema.methods.clearCart = function() {
+  this.cart = { items: [] };
+  return this.save();
+};
+
+module.exports = mongoose.model('User', userSchema);
